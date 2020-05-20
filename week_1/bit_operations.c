89e75220801c11a303d92326444ae458c35f44e5
jessicalally <jessica.lally19@imperial.ac.uk>
2020-05-20 18:54:44 +0100

    Completed bit operations tutorial

--- week_1/bit_operations.c
+++ week_1/bit_operations.c
@@ -18,10 +18,69 @@ void printBits(uint32_t x) {
   printf("\n");
 }
 
+uint16_t first(uint32_t x) {
+  int mask = 0xffff0000;
+
+  x = x & mask;
+  x = x >> 16;
+
+  uint16_t top_16 = (uint16_t) x;
+  return top_16;
+}
+
+uint16_t second(uint32_t x) {
+  int mask = 0x0000ffff;
+
+  x = x & mask;
+
+  uint16_t bottom_16 = (uint16_t) x;
+  return bottom_16;
+}
+
+void printBits16(uint16_t x) {
+  uint16_t mask = 1 << 15;
+
+  for (int i = 0; i < 16; i++) {
+    printf("%i", (x & mask) != 0);
+    mask = mask >> 1;
+  }
+  printf("\n");
+}
+
+void printSBits16(int16_t x) {
+ uint16_t mask = 1 << 15;
+
+  for (int i = 0; i < 16; i++) {
+    printf("%i", (x & mask) != 0);
+    mask = mask >> 1;
+  }
+  printf("\n");
+}
+
+
 int main(void) {
   uint32_t n = 1288243249;
 
   printBits(n);
+
+  printBits16(first(n));
+  printBits16(second(n));
+
+  printBits(first(n));
+  printBits(second(n));
+
+  int16_t signedFirst = first(n);
+
+  printf("%u\n", first(n));
+  printf("%d\n", signedFirst);
+
+  signedFirst = first(n) << 1;
+
+  printf("%u\n", first(n));
+  printf("%d\n", signedFirst);
+
+  printBits16(first(n));
+  printSBits16(signedFirst);
 
   return 0;
 }
